f79380a2f6f29e360a20cb0ce8dea61fc581570e
Ali Afshar <aafshar@gmail.com>
2007-11-29 19:20:16 +0000

    [project @ aafshar@gmail.com-20071129192016-25f4zrniiewz5u0e] cache controllers

--- example/example.py
+++ example/example.py
@@ -8,7 +8,7 @@ from glashammer import GlashammerSite, Response, Service, Controller, \
     Rule, UserPermission, EndpointLink
 
 config = {
-    'DB_URII': 'sqlite:test.sqlite',
+    'DB_URI': 'sqlite:test.sqlite',
 }
 
 # Controller
