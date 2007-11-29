adcdbe406ef5baeec0b5e800fcac5a0c68f724b8
Ali Afshar <aafshar@gmail.com>
2007-11-29 20:42:48 +0000

    [project @ aafshar@gmail.com-20071129204248-l2tvrckddlcum16z] removed instance stuff form controller and left only static stuff

--- glashammer/controller.py
+++ glashammer/controller.py
@@ -20,9 +20,6 @@ class Controller(object):
     def __init__(self, site,):
         self.site = site
         self.jinja = self.site.jinja_service.env
-        #self.session = self.environ['werkzeug.session']
-        #self.user_id = self.environ.get('glashammer.user_id')
-        #self.map_adapter = self.site.routing_service.bind_to_environ(environ)
 
     def __before__(self, req, *args):
         pass
@@ -30,8 +27,8 @@ class Controller(object):
     def __after__(self, req, *args):
         pass
 
-    def create_template_response(self, name, **kw):
-        return TemplateResponse(self.site, name, kw, req=self.req, controller=self)
+    def create_template_response(self, req, name, **kw):
+        return TemplateResponse(self.site, name, kw, req=req, controller=self)
 
     def list_feature_providers(self, feature):
         return self.site.feature_service.list(feature)
