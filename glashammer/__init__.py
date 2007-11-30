dc4a137cd79b9d4acee8a9b273567c6f6d18e4af
Ali Afshar <aafshar@gmail.com>
2007-11-30 01:52:27 +0000

    [project @ aafshar@gmail.com-20071130015227-fwl0yhjw9bqtsal5] added NotFoundResponse to namespace

--- glashammer/__init__.py
+++ glashammer/__init__.py
@@ -31,7 +31,8 @@ from glashammer.controller import Controller
 from werkzeug.routing import Rule
 from glashammer.ui import EndpointLink
 from glashammer.application import GlashammerSite
-from glashammer.utils import Request, Response, TemplateResponse, sibpath
+from glashammer.utils import Request, Response, TemplateResponse, sibpath, \
+    NotFoundResponse
 from glashammer.testing import TestController
 from glashammer.auth import UserPermission
 from glashammer.processors import ResponseProcessor, RequestProcessor
