81ad9c6eaa29275185ff5dffbefe5979314c275e
Ali Afshar <aafshar@gmail.com>
2007-11-30 01:02:19 +0000

    [project @ aafshar@gmail.com-20071130010219-3bb13xk4hu44r4sq] added package data to the setup.py script

--- setup.py
+++ setup.py
@@ -1,7 +1,27 @@
+"""
+Setup script for Glashammer.
+"""
+
 from distutils.core import setup
 
 
 setup(
+
     name='Glashammer',
-    packages=['glashammer'],
+
+
+    packages = [
+        'glashammer'
+    ],
+
+    package_dir = {
+        'glashammer': 'glashammer'
+    },
+
+    package_data = {
+        'glashammer': [
+            '*_public/*/*',
+            '*_templates/*'
+        ]
+    },
 )
