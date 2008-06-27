98e85fe786dcda9fc29a6c8024eaa5bae9d08f58
Ali Afshar <ali@horse>
2008-06-27 01:31:49 +0100

    [project @ ali@horse-20080627003149-inf17yot1c8n3s78] removed some empty files

--- example/example.py
+++ example/example.py
@@ -1,20 +1,19 @@
 
 from os.path import dirname
 
-from gh.application import make_app, run_very_simple, render_response
+from glashammer.application import make_app, run_very_simple, render_response
 
-from werkzeug import Response
+FOLDER = dirname(__file__)
 
 def index_view(req):
-    #return Response(u'Hello World')
     return render_response('hello.jinja')
 
 def setup(app):
     app.add_url_rule('/', 'example/hello', view=index_view)
-    app.add_template_searchpath(dirname(__file__))
+    app.add_template_searchpath(FOLDER)
 
 if __name__ == '__main__':
-    app = make_app(setup, dirname(__file__))
+    app = make_app(setup, FOLDER)
     run_very_simple(app)
 
 
