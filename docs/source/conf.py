3643ef15b67596ea40226c30d8efec19b4fe3d01
Ali Afshar <aafshar@gmail.com>
2008-07-26 12:06:14 +0100

    [project @ aafshar@gmail.com-20080726110614-awxlbpi3yr32dltg] updated documentation for 0.4.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -94,12 +94,12 @@ html_short_title = 'Glashammer'
 
 # The name of an image file (within the static path) to place at the top of
 # the sidebar.
-html_logo = 'glashammer-logo.png'
+html_logo = '_static/glashammer-logo.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = 'glashammer-icon.ico'
+html_favicon = '_static/glashammer-icon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
