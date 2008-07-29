7b72ee31c2ea742bd3c9535318daec7eeadf6e26
Ali Afshar <ali@rogue>
2008-07-29 14:26:42 +0100

    [project @ ali@rogue-20080729132642-whez62t6t5savt11] use GPL license

--- setup.py
+++ setup.py
@@ -6,10 +6,12 @@ from distutils.core import setup
 
 
 setup(
-    name='Glashammer',
 
+    # Required metadata
+    name='Glashammer',
     version='0.1',
 
+    # package information
     packages = [
         'glashammer', 'glashammer.bundles', 'glashammer.bundles.auth',
         'glashammer.bundles.forms', 'glashammer.bundles.i18n',
@@ -29,4 +31,20 @@ setup(
             'templates/*.html'
         ]
     },
+
+    # Additional pypi metadata
+    description = 'Full stack web framework',
+
+    long_description = ('Glashammer is a Python web framework with an emphasis'
+    ' on simplicity, flexibility, and extensibility. It is built atop excellent'
+    ' components and reinvents zero wheels.'),
+
+    url = 'http://glashammer.welterde.de',
+
+    author='Ali Afshar',
+    author_email='aafshar@gmail.com',
+
+    classifiers=[
+          'Development Status :: 4 - Beta',
+    ]
 )
