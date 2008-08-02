12c9bdb098c13d5ae5bbbd08b12be4510dd3ba5d
Ali Afshar <aafshar@gmail.com>
2008-08-02 12:22:47 +0100

    [project @ aafshar@gmail.com-20080802112247-kk2y27hq6cfohgn4] added a firephp example

---
+++ examples/firephp/run.py
@@ -0,0 +1,23 @@
+
+
+from os.path import dirname
+
+from glashammer import make_app, run_very_simple, Response
+
+
+def hello_view(req):
+    return Response('<h1>Hello World</h1>')
+
+def setup(app):
+    app.add_url('/', endpoint='hello/index', view=hello_view)
+
+    from glashammer.bundles.firephp import setup_app
+    app.add_setup(setup_app)
+
+def create_app():
+    app =  make_app(setup, dirname(__file__))
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    run_very_simple(app)
