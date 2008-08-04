9be63dfcc83f20deb392ab2a7428b0ba2731c186
Ali Afshar <ali@rogue>
2008-08-04 16:04:20 +0100

    [project @ ali@rogue-20080804150420-vcg2he5v5p664b8r] moved rest stuff out of json utils

---
+++ glashammer/utils/rest.py
@@ -0,0 +1,43 @@
+
+
+"""
+REST services for Glashammer
+"""
+
+class RestService(object):
+    """
+    Provides hooks for mounting REST on an endpoint.
+    """
+
+    def __call__(self, req, **kw):
+        if req.method == 'GET':
+            c = self.get
+        elif req.method == 'POST':
+            c = self.post
+        elif req.method == 'PUT':
+            c = self.put
+        elif req.method == 'DELETE':
+            c = self.delete
+        else:
+            c = None
+        if c:
+            return self.modifier(c(req, **kw))
+        else:
+            return NotFound()
+
+    def modifier(self, reponse):
+        return response
+
+    def post(self, req, **kw):
+        return NotFound()
+
+    def get(self, req, **kw):
+        return NotFound()
+
+    def put(self, req, **kw):
+        return NotFound()
+
+    def delete(self, req, **kw):
+        return NotFound()
+
+
