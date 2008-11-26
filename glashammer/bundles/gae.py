b624bf32c4861af184985f1280b4f19de842e336
Ali Afshar <aafshar@gmail.com>
2008-11-26 14:00:41 +0000

    caching for gae

--- glashammer/bundles/gae.py
+++ glashammer/bundles/gae.py
@@ -11,18 +11,29 @@
 
 from wsgiref.handlers import CGIHandler
 
-from google.appengine.api import users
+from werkzeug.contrib.cache import MemcachedCache, BaseCache
+
+from google.appengine.api import users, memcache
 from google.appengine.ext import db
 
 from glashammer.utils import local, get_request, redirect
 from glashammer.application import make_app
 
+
 def get_gae_user():
     return local.gae_user
+
+
+def get_gae_cache():
+    return local.gae_cache
+
 
 def gae_local_processor(req):
     user = users.get_current_user()
     local.gae_user = user
+    cache = GaeMemcachedCache()
+    local.gae_cache = cache
+
 
 def gae_user_only_view(f):
     def wrapped(req, **kw):
@@ -32,6 +43,7 @@ def gae_user_only_view(f):
             return f(req, **kw)
     return wrapped
 
+
 def gae_admin_only_view(f):
     def wrapped(req, **kw):
         if not users.is_current_user_admin():
@@ -40,16 +52,32 @@ def gae_admin_only_view(f):
             return f(req, **kw)
     return wrapped
 
+
 def redirect_gae_login_url():
     req = get_request()
     return redirect(users.create_login_url(req.url))
 
-def setup_gae(app):
+
+from google.appengine.api import memcache
+
+class GaeMemcachedCache(MemcachedCache):
+    """A werkzeug memcached object for GAE"""
+
+    def __init__(self, timeout=300):
+        BaseCache.__init__(self, timeout)
+        self._client = memcache.Client()
+
+
+def setup_gae(app, cache_manage_url=None):
     app.connect_event('request-start', gae_local_processor)
 
+    if cache_url
+
+
 def make_gae_app(setup_func):
-    app = make_app(setup_func, '.') 
+    app = make_app(setup_func, '.')
     return app
+
 
 def make_and_run_gae_app(setup_func):
     app = make_gae_app(setup_func)
