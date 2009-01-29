46dbf0a0a6dba4bf436c9fe71fceded3ede857b0
Ali Afshar <aafshar@gmail.com>
2009-01-29 12:29:31 +0000

    use more template extensions

--- glashammer/utils/templating.py
+++ glashammer/utils/templating.py
@@ -4,7 +4,8 @@ from jinja2 import Environment, FileSystemLoader, TemplateNotFound
 def create_template_environment(searchpaths=[], globals={}, filters={}, tests={}):
     env = Environment(
         loader=FileSystemLoader(searchpaths),
-        extensions=['jinja2.ext.i18n']
+        extensions=['jinja2.ext.i18n', 'jinja2.ext.do',
+                    'jinja2.ext.loopcontrols']
     )
     env.globals.update(globals)
     env.filters.update(filters)
