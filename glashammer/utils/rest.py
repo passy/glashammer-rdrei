c580c7a13d91eddac4a26cafcc5a46db5d58c6c5
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2009-02-22 11:31:50 +0100

    refactored rest base class

--- glashammer/utils/rest.py
+++ glashammer/utils/rest.py
@@ -1,4 +1,4 @@
-
+from werkzeug.exceptions import MethodNotAllowed
 
 """
 REST services for Glashammer
@@ -8,36 +8,34 @@ class RestService(object):
     """
     Provides hooks for mounting REST on an endpoint.
     """
+    #XXX: implement a sane default for options
+    possible_methods = 'GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'
 
     def __call__(self, req, **kw):
-        if req.method == 'GET':
-            c = self.get
-        elif req.method == 'POST':
-            c = self.post
-        elif req.method == 'PUT':
-            c = self.put
-        elif req.method == 'DELETE':
-            c = self.delete
+        handler = self._get_handler(req)
+        if handler:
+            response = handler(req, **kw)
+            return self.modifier(response)
         else:
-            c = None
-        if c:
-            return self.modifier(c(req, **req.args))
-        else:
-            return NotFound()
+            return MethodNotAllowed(valid_methods=self._get_methods())
+
+    @staticmethod
+    def _nice_name(method):
+        return method.tolower().replace('-','_')
+
+    def _get_methods(self):
+        methods = []
+        for method in self.possible_methods:
+            if getattr(self, self._nice_name(method), None) is not None:
+                options.append(method)
+        return methods
+
+    def _get_handler(self, req):
+        if req.method not in self.possible_methods:
+            return
+        nice_name = self._nice_name(req.method)
+        return getattr(self, nice_name, None)
 
     def modifier(self, reponse):
         return response
-
-    def post(self, req, **kw):
-        return NotFound()
-
-    def get(self, req, **kw):
-        return NotFound()
-
-    def put(self, req, **kw):
-        return NotFound()
-
-    def delete(self, req, **kw):
-        return NotFound()
-
 
