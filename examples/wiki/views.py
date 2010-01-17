b925b21aa12276396cf4de7b76c263a534732eb8
Ali Afshar <aafshar@gmail.com>
2010-01-17 11:52:59 +0000

    Updates to declared example application.

--- examples/wiki/views.py
+++ examples/wiki/views.py
@@ -4,17 +4,21 @@ from glashammer.utils import render_response, redirect_to
 from models import Page, Revision, session
 
 
-def view_index(req):
-    return render_response('index.jinja')
-
 def _get_page(name):
     return session.query(Page).get(name)
+
+
+def view_index(req):
+    pages = session.query(Page)
+    return render_response('index.jinja', pages=pages)
+
 
 def view_show(req, page_name):
     page = _get_page(page_name)
     if page is None:
         return redirect_to('edit', page_name=page_name)
     return render_response('show.jinja', page=page)
+
 
 def view_edit(req, page_name):
     page = _get_page(page_name)
@@ -27,6 +31,16 @@ def view_edit(req, page_name):
         session.commit()
         return redirect_to('show', page_name=page.name)
     else:
-        return render_response('edit.jinja', page=page)
+        if page is not None:
+            page_name = page.name
+        return render_response('edit.jinja', page=page, page_name=page_name)
+
+
+def view_new(req):
+    page_name = req.args.get('page_name')
+    if page_name:
+        return redirect_to('edit', page_name=page_name)
+    else:
+        return redirect_to('index')
 
 
