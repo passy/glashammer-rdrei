fde269a251987e1438d0828865e08c566c7bd45c
Ali Afshar <aafshar@gmail.com>
2010-01-27 09:43:10 +0000

    Updated example models for asier API

--- examples/wiki/models.py
+++ examples/wiki/models.py
@@ -2,27 +2,21 @@
 from sqlalchemy import *
 from sqlalchemy.orm import *
 
-from glashammer.bundles.sqlalchdb import metadata, session
-
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base(metadata=metadata)
+from glashammer.bundles.sqlalchdb import ModelBase
 
 
-class Page(Base):
+class Page(ModelBase):
 
     __tablename__ = 'pages'
-
     name = Column(String, primary_key=True)
 
 
-class Revision(Base):
+class Revision(ModelBase):
 
     __tablename__ = 'revision'
     id = Column(Integer, primary_key=True)
     page_name = Column(String, ForeignKey('pages.name'))
-    text = Column(UnicodeText)
-
     page = relation(Page, backref='revisions')
+    text = Column(UnicodeText)
 
 
